61d7a7fa4b86cfe5ee4f3b1caea4970299490db1
SergioMaturana <smaturana10@gmail.com>
2026-09-15 12:37:41 +0200

    Modulo 0: ejercicio describir_persona

---
+++ modulo_0/describir_persona.py
@@ -0,0 +1,16 @@
+#Escribe una función describir_persona(nombre, edad) que:
+
+#Devuelva un string usando f-string: "X tiene Y años"
+#Si edad < 18, añada " (menor de edad)" al final
+
+#Pruébala con un par de valores.
+
+def describir_persona(nombre, edad):
+    if edad < 18:
+        menor = " (menor de edad)"
+    else:
+        menor = ""
+    return f"{nombre} tiene {edad} años{menor}"
+
+print(describir_persona("Sergio", 22))
+print(describir_persona("Pedro", 16))
