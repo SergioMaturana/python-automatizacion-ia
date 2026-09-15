4d0f7d14e12ce0d5ec7a4c2b65995102fff2ef9e
SergioMaturana <smaturana10@gmail.com>
2026-09-15 17:01:03 +0200

    operadores

---
+++ modulo_0/Practica/A4_operadores_aritmeticos_y_comparacion.py
@@ -0,0 +1,19 @@
+a = 10
+b = 3
+
+# Aritméticos
+print(a + b)     # 13
+print(a - b)     # 7
+print(a * b)     # 30
+print(a / b)     # 3.333... (división normal, siempre da float)
+print(a // b)    # 3 (división entera, descarta decimales)
+print(a % b)     # 1 (resto/módulo)
+print(a ** b)    # 1000 (potencia)
+
+# Comparación (devuelven True/False)
+print(a == b)    # False (igual a)
+print(a != b)    # True (distinto de)
+print(a > b)     # True (mayor que)
+print(a < b)     # False (menor que)
+print(a >= b)    # True (mayor o igual)
+print(a <= b)    # False (menor o igual)
