e227d4dbd84cf6a8edc7f177a08f3d4fd92fd7cd
SergioMaturana <smaturana10@gmail.com>
2026-09-15 17:39:08 +0200

    par impar

---
+++ modulo_0/Ejercicios/Par_impar.py
@@ -0,0 +1,11 @@
+# Escribe un pequeño script que:
+
+# Tenga una variable numero con cualquier valor entero
+# Compruebe si es par o impar, e imprima "Es par" o "Es impar"
+
+numero = 3
+
+if numero % 2 == 0:
+    print("Es par")
+else:
+    print("Es impar")
