5de1d7f362e73a7ffb7cca57af794b671a4c36fe
SergioMaturana <smaturana10@gmail.com>
2026-09-16 17:57:50 +0200

    listas

---
+++ modulo_0/Practica/C1_lista.py
@@ -0,0 +1,7 @@
+frutas = ["manzana", "pera", "uva"]
+
+print(frutas)           # ['manzana', 'pera', 'uva']
+print(frutas[0])         # "manzana" (primer elemento, índice 0)
+print(frutas[1])         # "pera"
+print(frutas[-1])        # "uva" (índice negativo = desde el final)
+print(len(frutas))       # 3 (cuántos elementos tiene)
