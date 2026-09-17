b5b53a5a25ca58c8d41a4fe636ae1db60d093b3c
SergioMaturana <smaturana10@gmail.com>
2026-09-17 12:48:16 +0200

    lista de diccionarios

---
+++ modulo_0/Practica/D4_lista_diccoionarios.py
@@ -0,0 +1,8 @@
+personas = [
+    {"nombre": "Sergio", "edad": 22},
+    {"nombre": "Pedro", "edad": 16},
+    {"nombre": "Ana", "edad": 30},
+]
+
+for p in personas:
+    print(p["nombre"], p["edad"])
